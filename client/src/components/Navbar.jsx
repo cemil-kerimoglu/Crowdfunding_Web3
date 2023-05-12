b5f90d69a3edfd6df36c1c0e034bf9233a86f7e4
Cemil Kerimoglu <cemil.a.kerimoglu@gmail.com>
2023-05-12 13:39:26 +0200

    create the form to submit campaigns

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -52,7 +52,7 @@ export const Navbar = () => {
       <div className="sm:hidden flex justify-between items-center relative">
         <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
           <img
-            src={thirdweb}
+            src={logo}
             alt="user"
             className="w-[60%] h-[60%] object-contain"
           />
@@ -63,10 +63,11 @@ export const Navbar = () => {
           className="w-[34px] h-[34px] object-contain cursor-pointer"
           onClick={() => setToggleDrawer((prev) => !prev)}
         />
+
         <div
           className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${
-            !toggleDrawer ? "translate-y-[100vh]" : "translate-y-0"
-          } translate-all duration-700`}
+            !toggleDrawer ? "-translate-y-[100vh]" : "translate-y-0"
+          } transition-all duration-700`}
         >
           <ul className="mb-4">
             {navlinks.map((link) => (
